839acadb19abdc380f5b3b5b99308922b98e58cf
Katherine <kathynoelle24@gmail.com>
2020-02-24 21:38:45 +0800

    prototype

--- js/index.js
+++ js/index.js
@@ -1,17 +1,10 @@
-//how to define a class
+//prototype
 
-class Student{
-    constructor(name, course, section){ //argument
-        this.name = name; //parameter
-        this.course = course;
-        this.section = section;
-    }
+let stud1 = {name:"katherine uy", course:"IT", section:"2ITF", 
+    study:function(subject){
+        console.log(`${this.name} is enrolled in ${subject}`);
+    }};
 
-    enroll(subject){
-        console.log(`${this.name}, ${this.course} is enrolled in section ${this.sectionn} she is studying ${subject}`);
-    }
-}
+let stud2 = {name:"zachary timmothy", course:"IT", section:"2ITF"};
 
-//create an object
-let stud1 = new Student("Katherine Uy", "IT", "2ITF");
-stud1.enroll("ICS2609"); 
+stud1.study("ICS2609");
