6dbbf92757ba8304c9dd56f1dbf7429425ab273e
Katherine <kathynoelle24@gmail.com>
2020-02-24 21:41:14 +0800

    call function demo

--- js/index.js
+++ js/index.js
@@ -1,21 +1,10 @@
-//constructor function
+// call function demo
 
-function Student(name, course, section){
-    this.name = name;
-    this.course = course;
-    this.section = section;
-}
+let stud1 = {name:"juan dela cruz"};
+let stud2 = {name:"pedro cruz"};
 
-Student.prototype.study = function(subject){
+let study = function(subject){
     console.log(`${this.name} is enrolled in ${subject}`);
 }
 
-Student.prototype.school = "UST";
-
-let stud1 = new Student("katherine uy", "IT", "2ITF");
-let stud2 = new Student("zachary timmothy", "IT", "2ITF");
-
-stud1.study("Biology");
-stud2.study("Algebra");
-
-console.log(stud1.school);
+study.call(stud2, "Science");
